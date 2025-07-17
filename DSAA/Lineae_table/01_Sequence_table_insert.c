05ab6706ed8af8ad03da6d86972be94b02de0fed
heiweilu <273925452@qq.com>
2025-07-17 22:55:27 +0800

    01
    01
    
    Signed-off-by: heiweilu <273925452@qq.com>

---
+++ DSAA/Lineae_table/01_Sequence_table_insert.c
@@ -0,0 +1,30 @@
+typedef int Status;  /* 返回执行状态 */
+typedef int ElemType;  /* 元素类型，这里假设为int */
+
+typedef struct {
+    ElemType data[MAXSIZE];  /* 存储元素的数组 */
+    int length;              /* 当前顺序表长度 */
+}SqList;
+
+#define OK 1       /* 执行成功 */
+#define ERROR 0    /* 执行失败 */
+#define MAXSIZE 100  /* 顺序表最大长度 */
+
+Status ListInsert(SqList *L, int i, ElemType e)
+{
+    if(L->length == MAXSIZE)    /* 顺序表已满 */
+        return ERROR;
+
+    if(i < 1 || i > L->length + 1) /* 插入位置不合法 */
+        return ERROR; 
+
+    if(i <= L->length) /* 如果插入位置不在表尾。因为表尾不需要移动元素 */
+    {
+        for(int k = L->length - 1; k > i - 1; k--) /* 移动元素 */
+            L->data[k + 1] = L->data[k];
+    }
+    L->data[i - 1] = e; /* 插入新元素 */
+    L->length++; /* 更新顺序表长度 */
+
+    return OK; /* 返回成功状态 */
+} 
