ca15d89b8d669d11f139c6571dd973ac453fa0ab
heiweilu <273925452@qq.com>
2025-07-21 16:00:40 +0800

    code
    01,02,03
    
    Signed-off-by: heiweilu <273925452@qq.com>

--- DSAA/Lineae_table/01_Sequence_table_insert.c
+++ DSAA/Lineae_table/01_Sequence_table_insert.c
@@ -1,57 +1,56 @@
 #include <stdio.h>
 
-typedef int Status;  /* 返回执行状态 */
-typedef int ElemType;  /* 元素类型，这里假设为int */
+#define OK 1        /* 执行成功 */
+#define ERROR 0     /* 执行失败 */
+#define MAXSIZE 100 /* 顺序表最大长度 */
 
-#define OK 1       /* 执行成功 */
-#define ERROR 0    /* 执行失败 */
-#define MAXSIZE 100  /* 顺序表最大长度 */
+typedef int Status;   /* 返回执行状态 */
+typedef int ElemType; /* 元素类型，这里假设为int */
 
 typedef struct {
-    ElemType data[MAXSIZE];  /* 存储元素的数组 */
-    int length;              /* 当前顺序表长度 */
-}SqList;
+  ElemType data[MAXSIZE]; /* 存储元素的数组 */
+  int length;             /* 当前顺序表长度 */
+} SqList;
 
-Status ListInsert(SqList *L, int i, ElemType e)
-{
-    if(L->length == MAXSIZE)    /* 顺序表已满 */
-        return ERROR;
+Status ListInsert(SqList *L, int i, ElemType e) {
+  if (L->length == MAXSIZE) /* 顺序表已满 */
+    return ERROR;
 
-    if(i < 1 || i > L->length + 1) /* 插入位置不合法 */
-        return ERROR; 
+  if (i < 1 || i > L->length + 1) /* 插入位置不合法 */
+    return ERROR;
 
-    if(i <= L->length) /* 如果插入位置不在表尾。因为表尾不需要移动元素 */
-    {
-        for(int k = L->length - 1; k >= i - 1; k--) /* 移动元素 */
-            L->data[k + 1] = L->data[k];
-    }
-    L->data[i - 1] = e; /* 插入新元素 */
-    L->length++; /* 更新顺序表长度 */
+  if (i <= L->length) /* 如果插入位置不在表尾。因为表尾不需要移动元素 */
+  {
+    for (int k = L->length - 1; k >= i - 1; k--) /* 移动元素 */
+      L->data[k + 1] = L->data[k];
+  }
+  L->data[i - 1] = e; /* 插入新元素 */
+  L->length++;        /* 更新顺序表长度 */
 
-    return OK; /* 返回成功状态 */
-} 
+  return OK; /* 返回成功状态 */
+}
 
-int main() {
-    SqList L = {{1, 2, 3, 4, 5}, 5}; /* 初始化顺序表 */
-    ElemType newElement = 10; /* 要插入的新元素 */
-    int pos = 3; /* 要插入的位置 */
+int main(void) {
+  SqList L = {{1, 2, 3, 4, 5}, 5}; /* 初始化顺序表 */
+  ElemType newElement = 10;        /* 要插入的新元素 */
+  int pos = 3;                     /* 要插入的位置 */
 
-    printf("插入前顺序表的元素为：");
-    for(int i = 0; i < L.length; i++) {
-        printf("%d ", L.data[i]);
+  printf("插入前顺序表的元素为：");
+  for (int i = 0; i < L.length; i++) {
+    printf("%d ", L.data[i]);
+  }
+  printf("\n");
+
+  if (ListInsert(&L, pos, newElement) == OK) {
+    printf("在位置 %d 插入元素 %d 成功！\n", pos, newElement);
+    printf("插入后顺序表的元素为：");
+    for (int i = 0; i < L.length; i++) {
+      printf("%d ", L.data[i]);
     }
     printf("\n");
+  } else {
+    printf("在位置 %d 插入元素失败！\n", pos);
+  }
 
-    if(ListInsert(&L, pos, newElement) == OK) {
-        printf("在位置 %d 插入元素 %d 成功！\n", pos, newElement);
-        printf("插入后顺序表的元素为：");
-        for(int i = 0; i < L.length; i++) {
-            printf("%d ", L.data[i]);
-        }
-        printf("\n");
-    } else {
-        printf("在位置 %d 插入元素失败！\n", pos);
-    }
-
-    return 0;
+  return 0;
 }
---
+++ DSAA/Lineae_table/03_Linke_List_get.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define OK 1
+#define ERROR 0
+
+typedef int Status;   /* 状态信息 */
+typedef int ElemType; /* 元素类型 */
+
+typedef struct Node {
+  int data;          /* 数据域*/
+  struct Node *next; /* 指针域，指向下一个结点 */
+} Node;
+
+Status GetElem(Node *L, int i, ElemType *e) {
+  int j = 1;         /* 计数器 */
+  Node *p = L->next; /* p指向头结点的下一个结点 */
+
+  while (p && j < i) {
+    p = p->next; /* p指向下一个结点 */
+    j++;         /* 计数器加1 */
+  }
+
+  if (!p) /* 如果p为空，说明第i个元素不存在 */
+  {
+    return ERROR; /* 返回错误状态 */
+  }
+
+  *e = p->data; /* 取出数据 */
+
+  return OK; /* 返回成功状态 */
+}
+
+int main(void) {
+  Node head = {0, NULL}; // 初始化头结点
+  Node second = {2, NULL};
+  Node third = {3, NULL};
+
+  head.next = &second;  // 头结点指向第二个结点
+  second.next = &third; // 第二个结点指向第三个结点
+
+  ElemType e;
+  if (GetElem(&head, 2, &e) == OK) {
+    printf("Element at position 2: %d\n", e);
+  } else {
+    printf("Error retrieving element at position 2.\n");
+  }
+
+  return 0;
+}
